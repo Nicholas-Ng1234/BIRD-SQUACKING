58e8acf46d11dbbf05955d85248bafcfca7d067b
Nicholas-Ng1234 <102217183+Nicholas-Ng1234@users.noreply.github.com>
2022-03-23 16:50:06 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function change() {
+  document.getElementById("img").src = "ostritch pic 2.jpg";
+  document.getElementById("squacksound").play();
+}
+
+function changeto() {
+  document.getElementById("img").src = "ostrich pic 1.jpg";
+}
